7023ec95fc08a3989b53cba857ddb54158f44bfe
Abigail210 <54431473+Abigail210@users.noreply.github.com>
2021-10-24 18:56:30 -0500

    Create Tokens.java

---
+++ Tokens.java
@@ -0,0 +1,21 @@
+package codigo;
+
+/**
+ *
+ * @author abihs
+ */
+public enum Tokens {
+    Linea,
+    ORG,
+    EQU,
+    FCB,
+    END,
+    Gato,
+    Apostrofe,
+    SignoPeso,
+    Opcode,
+    DosPuntos,
+    Mnemonico,
+    Numero,
+    ERROR
+}
